25150447e93400a565007062d0594b05949c51f3
Ben Daniels <benjamindaniels21@gmail.com>
2021-12-18 11:40:25 -0500

    workshop task 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -24,4 +24,29 @@ campsiteRouter
     res.end("Deleting all campsites");
   });
 
+campsiteRouter
+  .route("/:campsiteId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(`Sending campsite with id of ${req.params.campsiteId}`);
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `This operation for adding campsite with id ${req.params.campsiteId} is not supported`
+    );
+  })
+  .put((req, res) => {
+    res.end(
+      `updating campsite with id ${req.body.name} and description as ${req.body.description} for id ${req.params.campsiteId}`
+    );
+  })
+  .delete((req, res) => {
+    res.end(`Deleting campsite with id ${req.params.campsiteId}`);
+  });
+
 module.exports = campsiteRouter;
